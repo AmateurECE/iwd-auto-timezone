15b881fbdc637026cb679a510df5b320769042b9
Ethan D. Twardy <ethan.twardy@gmail.com>
2022-12-29 11:45:41 -0600

    Add MIT license

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,34 @@
 //
 // AUTHOR:          Ethan D. Twardy <ethan.twardy@gmail.com>
 //
-// DESCRIPTION:     Entrypoint for the application
+// DESCRIPTION:     Application which updates the system time zone based on
+//                  Geo-IP data provided by IP-API.
 //
 // CREATED:         12/27/2022
 //
-// LAST EDITED:	    12/28/2022
+// LAST EDITED:	    12/29/2022
 //
-//////
+////
+// Copyright 2022, Ethan D. Twardy
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+////
 
 use core::time::Duration;
 use dbus::arg::Variant;
